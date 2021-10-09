b8001289e9b54ecd585f400644948925986115e9
Ribbinnn <r.supichaya29@gmail.com>
2021-10-09 23:49:48 +0700

    add login form

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import './Login.css'
+import { Row, Col, Form, Input, Checkbox, Button } from 'antd';
 
 function Login() {
     return(
-        <h1 className="text-center">
-            Login Page
-        </h1>
+        <div style={{width: "100%"}}>
+            <Row align="middle" justify="center" style={{height: "100%"}}>
+                <Col span={12}>
+                    <div className="login-div login-logo">
+                        Temp Logo Image
+                    </div>
+                </Col>
+                <Col span={12}>
+                    <div className="login-div">
+                        <Form layout="vertical">
+                            <Form.Item label="Username">
+                                <Input style={{width: "300px"}} />
+                            </Form.Item>
+                            <Form.Item label="Password">
+                                <Input type="password" style={{width: "300px"}} />
+                            </Form.Item>
+                            <Form.Item>
+                                <Checkbox>Remember me</Checkbox>
+                                <a style={{float: "right", color: "#de5c8e"}}>Forgot password?</a>
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" style={{width: "100%"}}>Sign in</Button>
+                            </Form.Item>
+                        </Form>
+                    </div>
+                </Col>
+            </Row>
+        </div>
     );
 }
 
