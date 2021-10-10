247e2fba40320bb54c276c888eb92593944a1088
phare <fairphare@gmail.com>
2021-10-11 01:16:59 +0700

    fix: button font size

--- src/Completed.js
+++ src/Completed.js
@@ -11,8 +11,8 @@ export default function Completed(props) {
   
     return (
       <div className="btn-column">
-        <CheckCircleOutlined style={{ fontSize: '200px', color: "#de5c8e" }}/>
-        <span style={{ fontSize: '30px', color: "#de5c8e", marginTop: 20, marginBottom: 10 }}> {title} </span>
+        <CheckCircleOutlined style={{ fontSize: '150px', color: "#de5c8e" }}/>
+        <span style={{ fontSize: '25px', color: "#de5c8e", marginTop: 20, marginBottom: 10 }}> {title} </span>
         {btnList.map((btn) => (
           <a href={btn.destination} key={btn.title}>
             <Button className="primary-btn" >{btn.title}</Button>
