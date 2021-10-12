7699a643beb235dd3f65807a084990ccbf19d5be
mmkanta <47110972+mmkanta@users.noreply.github.com>
2021-10-13 00:35:10 +0700

    add select project [upload record]

--- src/diagnosis/Diagnosis.js
+++ src/diagnosis/Diagnosis.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import { Steps, Button, Form, Input, Select, Collapse, Tag } from "antd";
+import { Steps, Button, Form, Input } from "antd";
 import "antd/dist/antd.css";
+import SelectProject from "./SelectProject";
 const { Step } = Steps;
-const { Panel } = Collapse;
-const { Option } = Select;
 
 const steps = [
   {
@@ -97,64 +96,6 @@ function SelectHN(props) {
       <div style={{ paddingTop: 30, paddingLeft: 60 }}>
         {props.HN && <SelectProject setProject={props.setProject} Project={props.Project}/>}
       </div>
-    </div>
-  );
-}
-
-function SelectProject(props) {
-  const setDefaultValue = () => {
-    for (let i=0; i < itemList.length; i++){
-      if (itemList[i].ProjectName === props.Project.ProjectName) return i;
-    }
-    return ""
-  }
-  const itemList = /* []call api get all project* */[
-    {
-      ProjectName: "COVID-19",
-      Task: "2D Classfication",
-      Classes: ["normal", "COVID-19"],
-      Description: "COVID-19 Evaluation from CXR and vital signs",
-      Requirement: ["Pulse rate", "Blood Pressure", "Temperature"],
-    },
-    {
-      ProjectName: "Pylon",
-      Task: "2D Classfication",
-      Classes: ["normal", "Tubercolosis"],
-      Description: "P'Plot Model",
-      Requirement: [],
-    },
-  ]; 
-  function handleChange(value) {
-    props.setProject(itemList[value])
-    console.log(`select value = ${value}`)
-  }
-  return (
-    <div style={{ minWidth: 450 }}>
-      <p className="project-lable" style={{ marginBottom: "8px" }}> Project </p>
-      <Select onChange={handleChange} dropdownStyle={{ borderRadius: 8 }} defaultValue={setDefaultValue}>
-        {itemList.map((item, i) => (
-          <Option key={i} value={i}>
-            {item.ProjectName}
-          </Option>
-        ))}
-      </Select>
-      {(props.Project !== "none") && <Collapse defaultActiveKey={["1"]} expandIconPosition="right" ghost>
-        <Panel key="1" header="Project information">
-          <div className="project-info">
-            <div>
-              Task : <Tag className="brown">{props.Project.Task}</Tag>
-            </div>
-            <div>
-              Classes : {(props.Project.Classes).map((item, i)=>(<Tag key={i} className="pink">{item}</Tag>))}
-            </div>
-            <div>Description: {props.Project.Description}</div>
-            <div>
-              Requirement :
-              {(props.Project.Requirement).map((item, i)=>(<ol key={i}>{item}</ol>))}
-            </div>
-          </div>
-        </Panel>
-      </Collapse> }
     </div>
   );
 }
