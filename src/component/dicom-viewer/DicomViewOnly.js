33b48676239b7e59de75f5d98f932f10af79fc1e
phare <fairphare@gmail.com>
2021-11-04 02:29:36 +0700

    fix: remove excess black area from dicom image

--- src/component/dicom-viewer/DicomViewOnly.js
+++ src/component/dicom-viewer/DicomViewOnly.js
@@ -11,29 +11,34 @@ const cornerstone = window.cornerstone;
 
 export default function DicomViewOnly(props) {
   const [loaded, setLoaded] = useState(false);
-  const [element, setElement] = useState(null);
 
   useEffect(() => {
-    var e = document.getElementById("dicomViewImage");
-    setElement(e);
-    setLoaded(false);
-    if (element) {
-      cornerstone.enable(element);
       loadDicom(props.img_url, props.img_source, displayImage);
-    }
-  }, [element,props.img_url]);
+  }, [props.img_url]);
 
   function displayImage(image) {
+    var img_ratio = {height:image.height, width:image.width}
+    var element = document.getElementById("dicomViewImage");
+    if (img_ratio.height > img_ratio.width){
+      element.style.width = `${(img_ratio.width/img_ratio.height)*props.size}px`
+      element.style.height = `${props.size}px`
+    }
+    else if (img_ratio.height < img_ratio.width){
+      element.style.height = `${(img_ratio.height/img_ratio.width)*props.size}px`
+      element.style.width = `${props.size}px`
+    }
+    cornerstone.enable(element);
     var viewport = cornerstone.getDefaultViewportForImage(element, image);
     cornerstone.displayImage(element, image, viewport);
     setLoaded(true);
   }
 
   return (
-    <div className="dicomImage-div" style={{ width: props.width }}>
+    <div className="dicomImage-div" style={{height: `${props.size}px`, width: `${props.size}px`}}>
       {!loaded && (
-        <div className="loading-dicom">
+        <div className="loading-dicom" style={{textAlign:"center"}}>
           <Spin indicator={LoadingIcon} />
+          <br />
           <br />
           <span style={{ fontSize: "medium" }}>Loading ...</span>
         </div>
@@ -41,7 +46,7 @@ export default function DicomViewOnly(props) {
       <div
         id="dicomViewImage"
         className="dicomImage"
-        style={{ height: props.height, width: props.width }}
+        style={{display:"relative"}}
       />
     </div>
   );
