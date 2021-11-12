59a21b5c6ba3dada463fac15bf6aaeb9848f2d2a
phare <fairphare@gmail.com>
2021-11-13 03:23:02 +0700

    feat: get dicom by accession no

--- src/api/image.js
+++ src/api/image.js
@@ -3,3 +3,7 @@ import { serverURL } from './config';
 export const getGradCam = (rid, finding) => {
     return `${serverURL}/image/?result_id=${rid}&finding=${finding}`
 }
+
+export const getDicomByAccessionNo = (ano) =>{
+    return `${serverURL}/image/?accession_no=${ano}`
+}
