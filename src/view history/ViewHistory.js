92c576c688b7097af8714a2855f1b7a71e5625fa
Ribbinnn <r.supichaya29@gmail.com>
2021-11-16 22:32:03 +0700

    view history: search with query params

--- src/view history/ViewHistory.js
+++ src/view history/ViewHistory.js
@@ -4,7 +4,8 @@ import { DownloadOutlined ,EditOutlined, DeleteOutlined, ReloadOutlined } from '
 import {viewHistory} from "../api/viewHistory"
 import SelectProject from "../component/SelectProject";
 import ImageModal from "../component/ImageModal";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
+import * as moment from 'moment';
 
 const { Option } = Select;
 
@@ -25,7 +26,14 @@ export default function ViewHistory() {
 }
 
 function HistoryLog(props) {
+
+    function useQuery() {
+        const { search } = useLocation();
+        return new URLSearchParams(search);
+    }
+
     const history = useHistory();
+    const queryString = useQuery();
     const [uploadedItem, setUploadedItem] = useState([])
     const status = ["in_progress", "annotated", "finalized"];
     const findings = [];
@@ -152,7 +160,7 @@ function HistoryLog(props) {
                                         let role = (JSON.parse(sessionStorage.getItem('user'))).role
                                         console.log((JSON.parse(sessionStorage.getItem('user'))).role, report)
                                         /* SHOW REPORT */
-                                        history.push(`/viewhistory/${role === "clinician" ? "view" : "edit"}/${report.pred_result_id}`)
+                                        history.push(`/viewhistory/${role === "clinician" ? "view" : "edit"}/${report.pred_result_id}/?${queryString}`)
                                     }}
                                 />
                                 <DeleteOutlined
@@ -173,61 +181,104 @@ function HistoryLog(props) {
     useEffect(() => {
         viewHistory(props.project).then((response) => {
             console.log(response);
+            // filter data by search query params
+            let filter_data = response.data.filter((item, i) => (
+                (queryString.get("patient_HN") === null ? true : item.hn === parseInt(queryString.get("patient_HN"))) &&
+                (queryString.get("status") === null ? true : item.status === queryString.get("status")) &&
+                (queryString.get("findings") === null ? true : item.finding === queryString.get("findings")) &&
+                (queryString.get("clinician") === null ? true : item.clinician_name === queryString.get("clinician")) &&
+                (queryString.get("from") === null ? true : new Date(item.createdAt) >= new Date(queryString.get("from"))) &&
+                (queryString.get("to") === null ? true : new Date(item.createdAt) <= new Date(queryString.get("to")))
+            ))
             // add key to each row & change date-time
-            for (const i in response.data) {
-                response.data[i]["key"] = (parseInt(i)+1).toString();
-                response.data[i].createdAt = new Date(response.data[i].createdAt).toLocaleString();
-                response.data[i].updatedAt = new Date(response.data[i].updatedAt).toLocaleString();
+            for (const i in filter_data) {
+                filter_data[i]["key"] = (parseInt(i)+1).toString();
+                filter_data[i].createdAt = new Date(filter_data[i].createdAt).toLocaleString();
+                filter_data[i].updatedAt = new Date(filter_data[i].updatedAt).toLocaleString();
             }
-            setUploadedItem(response.data)
+            setUploadedItem(filter_data);
         })
     }, [])
 
     return (
         <div>
-            <div>
-                <Form layout="inline">
-                    <Form.Item label="Patient's HN" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>
-                        <Input className="input-text" onChange="" style={{width:"200px"}} />
-                    </Form.Item>
-                    <Form.Item label="Status" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>                
-                        <Select className="search-component" defaultValue="All" onChange="">
+            <Form layout="inline">
+                <Form.Item name="patient_HN" label="Patient's HN" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>
+                    <Input
+                        className="input-text"
+                        defaultValue={queryString.get("patient_HN")}
+                        onChange={(item) => {
+                            item.target.value === "" ? queryString.delete("patient_HN") : queryString.set("patient_HN", item.target.value);
+                        }}
+                        style={{width:"200px"}} />
+                </Form.Item>
+                <Form.Item name="status" label="Status" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>                
+                    <Select
+                        className="search-component"
+                        defaultValue={queryString.get("status") === null ? "All" : queryString.get("status")}
+                        onChange={(value) => {
+                            queryString.set("status", status[value]);
+                        }}>
                             {status.map((status, i) => (
                             <Option key={i} value={i}>
                                 {status}
                             </Option>
                             ))}
-                        </Select>
-                    </Form.Item>
-                    <Form.Item label="Findings" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>                
-                        <Select className="search-component" defaultValue="All" onChange="">
+                    </Select>
+                </Form.Item>
+                <Form.Item name="findings" label="Findings" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>                
+                    <Select
+                        className="search-component"
+                        defaultValue={queryString.get("findings") === null ? "All" : queryString.get("findings")}
+                        onChange={(value) => {
+                            queryString.set("findings", findings[value]);
+                        }}>
                             {findings.map((findings, i) => (
                             <Option key={i} value={i}>
                                 {findings.ProjectName}
                             </Option>
                             ))}
-                        </Select>
-                    </Form.Item>
-                    <Form.Item label="Clinician" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>
-                        <Input className="input-text" onChange="" style={{width:"200px"}} />
-                    </Form.Item>
-                </Form>
-            </div>
-            <div>
-                <Form layout="inline">
-                    <Form.Item label="From" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>   
-                        <DatePicker /*onChange={onChangeFirstDate}*/ style={{width:"200px"}} />
-                    </Form.Item>
-                    <Form.Item label="To" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>
-                        <DatePicker /*onChange={onChangeLastDate}*/ style={{width:"200px"}} />
-                    </Form.Item>
-                    <Form.Item style={{marginLeft:"20px"}}>
-                        <Button className="primary-btn smaller" style={{marginTop:"32px"}} onClick="">
+                    </Select>
+                </Form.Item>
+                <Form.Item name="clinician" label="Clinician" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>
+                    <Input
+                        className="input-text"
+                        defaultValue={queryString.get("clinician")}
+                        onChange={(item) => {
+                            item.target.value === "" ? queryString.delete("clinician") : queryString.set("clinician", item.target.value);
+                        }}
+                        style={{width:"200px"}} />
+                </Form.Item>
+            </Form>
+            <Form layout="inline">
+                <Form.Item name="from" label="From" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>   
+                    <DatePicker
+                        defaultValue={queryString.get("from") === null ? null : moment(new Date(queryString.get("from")))}
+                        onChange={(date) => {
+                            date === null ? queryString.delete("from") : queryString.set("from", date.startOf('day').toDate().toLocaleString());
+                        }}
+                        style={{width:"200px"}} />
+                </Form.Item>
+                <Form.Item name="to" label="To" style={{display:"flex", flexDirection:"column", alignItems:"flex-start"}}>
+                    <DatePicker
+                        defaultValue={queryString.get("to") === null ? null : moment(new Date(queryString.get("to")))}
+                        onChange={(date) => {
+                            date === null ? queryString.delete("to") : queryString.set("to", date.startOf('day').toDate().toLocaleString());
+                        }}
+                        style={{width:"200px"}} />
+                </Form.Item>
+                <Form.Item style={{marginLeft:"20px"}}>
+                    <Button
+                        className="primary-btn smaller"
+                        style={{marginTop:"32px"}}
+                        onClick={() => {
+                            history.push(`/viewhistory/?${queryString}`);
+                            window.location.reload();
+                        }}>
                             Search
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </div>
+                    </Button>
+                </Form.Item>
+            </Form>
             <label
                 className="clickable-label"
                 style={{color: "#de5c8e", display: "flex", alignItems: "center", margin: "30px 0 8px 0"}}
