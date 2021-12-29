cdb9e956a3348f60b298e8d2fa5fb3076fb879b8
phare <fairphare@gmail.com>
2021-12-30 03:25:10 +0700

    fix: button style

--- src/view history/annotate/AnnotationModal.js
+++ src/view history/annotate/AnnotationModal.js
@@ -15,7 +15,13 @@ export default function AnnotationModal (props){
 
   return (
       <div style={{display: "inline-block"}}>
-        <Button type="link" onClick={showModal} icon={<HighlightOutlined className="clickable-icon"/>}>
+        <Button type="link" onClick={showModal} icon={<HighlightOutlined className="clickable-icon"/>} style={{
+                color: "#de5c8e",
+                fontSize: "medium",
+                fontWeight: "bold",
+                stroke: "#de5c8e",
+                strokeWidth: 30
+              }}>
             Annotate Image
         </Button>
         <Modal
