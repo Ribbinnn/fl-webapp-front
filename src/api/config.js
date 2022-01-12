67e6c216c1293c68d34c9bfa154eac684f4b8a25
phare <fairphare@gmail.com>
2022-01-12 16:26:32 +0700

    fix: annotate when hiding

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 /* HEROKU */
-export const serverURL = "https://flweb-capstone-back.herokuapp.com/api";
+// export const serverURL = "https://flweb-capstone-back.herokuapp.com/api";
 
 /* LOCALHOST + DOCKER */
-// export const serverURL = "http://localhost:5000/api";
+export const serverURL = "http://localhost:5000/api";
