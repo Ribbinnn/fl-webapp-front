70a350828522b0117f7e6a7dfbb2a1ea65277633
Ribbinnn <r.supichaya29@gmail.com>
2022-01-15 14:51:24 +0700

    diagnosis: select x-ray image: add date filter

--- src/diagnosis/Diagnosis.js
+++ src/diagnosis/Diagnosis.js
@@ -65,6 +65,9 @@ export default function Diagnosis() {
   // const [accessionNo, setAccessionNo] = useState("74");
   const [accessionNo, setAccessionNo] = useState(null);
   const [accessionNoIndex, setAccessionNoIndex] = useState([]);
+  const [fromDate, setFromDate] = useState(null);
+  const [toDate, setToDate] = useState(null);
+  const [pacsTableData, setPacsTableData] = useState(null);
   const [current, setCurrent] = useState(0);
   const selectMedicalRecordRef = useRef();
 
@@ -160,6 +163,12 @@ export default function Diagnosis() {
               setAccessionNo={setAccessionNo}
               accessionNoIndex={accessionNoIndex}
               setAccessionNoIndex={setAccessionNoIndex}
+              fromDate={fromDate}
+              setFromDate={setFromDate}
+              toDate={toDate}
+              setToDate={setToDate}
+              pacsTableData={pacsTableData}
+              setPacsTableData={setPacsTableData}
             />
           </div>
         )}
