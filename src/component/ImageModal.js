bc228f8e1c8ca72959799db9529511011de1e456
phare <fairphare@gmail.com>
2022-02-05 01:52:15 +0700

    change: preview png image instead of dicom

--- src/component/ImageModal.js
+++ src/component/ImageModal.js
@@ -1,35 +1,52 @@
 import React, { useState } from "react";
-import { Modal, Button } from 'antd';
-import { EyeOutlined } from '@ant-design/icons';
+import { Modal, Button, Image } from "antd";
+import { EyeOutlined } from "@ant-design/icons";
 import DicomViewOnly from "./dicom-viewer/DicomViewOnly";
 import { getDicomByAccessionNo } from "../api/image";
+import { getGradCam } from "../api/image";
 
-export default function ImageModal (props){
-  const [visible,setVisible] = useState(false)
+export default function ImageModal(props) {
+  const [visible, setVisible] = useState(false);
   const showModal = () => {
-    setVisible(true)
+    setVisible(true);
   };
 
   const handleCancel = () => {
-    setVisible(false)
+    setVisible(false);
   };
 
   return (
-      <div style={{display: "inline-block"}}>
-        <Button type="link" onClick={showModal} icon={<EyeOutlined className="clickable-icon" />}/>
-        <Modal
-          centered
-          destroyOnClose
-          className="img-modal"
-          visible={visible}
-          // title={`${props.ProcDescription} ${props.StudyDateTime}`}
-          title={props.StudyDateTime}
-          onCancel={handleCancel}
-          footer={null}
-          width="800px"
-        >
-          <DicomViewOnly img_url={getDicomByAccessionNo(props.AccessionNo)} img_source="wado" size={530}/>
-        </Modal>
-      </div>
-    )
+    <div style={{ display: "inline-block" }}>
+      <Button
+        type="link"
+        onClick={showModal}
+        icon={<EyeOutlined className="clickable-icon" />}
+      />
+      <Modal
+        centered
+        destroyOnClose
+        className="img-modal"
+        visible={visible}
+        // title={`${props.ProcDescription} ${props.StudyDateTime}`}
+        title={props.StudyDateTime}
+        onCancel={handleCancel}
+        footer={null}
+        width="800px"
+      >
+        {props.ReportID ? (
+          <Image
+            preview={false}
+            height={530}
+            src={getGradCam(props.ReportID, "original")}
+          />
+        ) : (
+          <DicomViewOnly
+            img_url={getDicomByAccessionNo(props.AccessionNo)}
+            img_source="wado"
+            size={530}
+          />
+        )}
+      </Modal>
+    </div>
+  );
 }
